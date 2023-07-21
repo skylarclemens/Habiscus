671c7fbdd88f24854162976bbc58a347edf4f819
Skylar <skylarc55@gmail.com>
2023-07-21 19:56:44 -0400

    Initial Commit

---
+++ Habiscus/HabiscusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabiscusApp.swift
+//  Habiscus
+//
+//  Created by Skylar Clemens on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct HabiscusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
